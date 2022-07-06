af5aadcbf80aef9cf77cf80d0ac11e5b397cf8e1
Lucas Reis <lucaschristian10@gmail.com>
2022-07-05 22:11:38 -0300

    feat: criar configmap para variaveis de ambiente

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT;
+
 app.get('/', (req, res) => {
   res.send('Hello World K8S !')
 })
-app.listen(3000, () => console.log('Server is up and running'));
+app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
